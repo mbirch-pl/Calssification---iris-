f6e3e85b381ebfe9b1ce457498dba73b6999dc3d
mbirch-pl <m.d.brzoza@gmail.com>
2020-11-02 02:15:29 +0100

    added classification models

---
+++ Decision_tree.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.utils import shuffle
+
+data = load_iris()
+
+X = data['data']
+y = data['target']
+
+X,y = shuffle(X,y,random_state=0)
+
+from  sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=0)
+
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion='entropy',random_state=0)
+classifier.fit(X_train,y_train)
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix,accuracy_score
+acc = accuracy_score(y_test,y_pred)
+cm = confusion_matrix(y_test, y_pred)
+
+print(acc,'\n',cm)
